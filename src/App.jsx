6d39d959ea8dee9de9b5b16f3b648538f7e5b2a8
Rama <RamaKhalidCS@outlook.com>
2025-10-29 18:56:26 +0300

    update homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
 import './App.css'
-import Home from './components/home/home'
+import HomePage from './components/HomePage/HomePage'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,7 @@ function App() {
       {/* Nav bar takes the user and the set user to show either login or logout buttons */}
       {/* <NavBar user={user} setUser={setUser}/> */}
       <Routes>
-        <Route path='/home' element={<Home />}/>
+        <Route path='/home' element={<HomePage />}/>
       </Routes>
     </Router>
   )
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+import './homeStyle.scss';
+
+
+
+function HomePage() {
+    const [users, setUSers] = useState([])
+    const [profileList, setProfileList] = useState([])
+    const LevelNames = {
+        B: 'Beginner',
+        I: 'Intermediate',
+        E: 'Expert'
+    }
+
+     async function getAllUSer() {
+        const response = await axios.get('http://127.0.0.1:8000/ss/home/')
+        console.log(response.data)
+        setUSers(response.data.user)
+        setProfileList(response.data.profile)
+    }
+    useEffect(() => {
+        getAllUSer()
+    }, [])
+
+    function getSkills(userID){
+       const userProfile = profileList.find(profile=> profile.user === userID)
+        if (userProfile)
+        return LevelNames[userProfile.level]
+    }
+
+  return (
+    <div>
+        <h1>Users:</h1>
+
+        <div className="container">
+        {
+            users.length
+                ?
+                users.map((user) => {
+                    return (
+                        <div className="card">
+                        <div className="content">
+                            <div className="img"><img src="https://unsplash.it/200/200"/></div>
+                            <div className="cardContent">
+                            <h3>{user.first_name} - {user.last_name}<br/>
+                            <span>level: {getSkills(user.id)}  </span></h3>
+                            {/* <span>level: {profileList.find(profile=> profile.user === user.id).level}</span></h3> */}
+                            </div>
+                        </div>
+                        <ul className="sci">
+                            <li style={{"--i":1}}>
+                            <a href="#"><i className="fa fa-envelope" aria-hidden="true"></i></a>
+                            </li>
+                        </ul>
+                </div>
+                    )
+                })
+                :
+                <h2>No Users</h2>
+            }
+            
+        </div>
+
+
+
+
+
+
+
+  
+
+    </div>
+  )
+}
+
+export default HomePage
+
+
+
+
+
